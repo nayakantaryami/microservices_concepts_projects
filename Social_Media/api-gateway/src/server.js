b5664894e650c763173409be849b1ec89bb6a3df
nayakantaryami <nayakantaryami324@gmail.com>
2025-08-24 05:39:53 +0530

    added proxy server routing for post-services

--- Social_Media/api-gateway/src/server.js
+++ Social_Media/api-gateway/src/server.js
@@ -8,6 +8,7 @@ const { RedisStore } = require("rate-limit-redis");
 const logger = require("./utils/logger");
 const proxy = require("express-http-proxy");
 const errorHandler = require("./middleware/errorHandler");
+const { validateToken } = require("./middleware/authMiddleware");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -43,6 +44,10 @@ app.use((req, res, next) => {
   //using json.stringify(req.body) will expose all the user credentials
   next();
 });
+
+
+
+
 const proxyOptions = {
   proxyReqPathResolver: (req) => {
     return req.originalUrl.replace(/^\/v1/, "/api");
@@ -54,16 +59,44 @@ const proxyOptions = {
       error: err.message,
     });
   },
-  useResDecorator: (proxyRes, proxyResData, userReq, userRes) => {
-    logger.info(
-      `Response received from identity-service: ${proxyRes.statusCode}`
-    );
-    return proxyResData;
-  },
+ 
 };
 
 //setting up the proxy for identity service
-app.use("/v1/auth", proxy(process.env.IDENTITY_SERVICE_URL, proxyOptions));
+app.use("/v1/auth", proxy(process.env.IDENTITY_SERVICE_URL, {
+    ...proxyOptions,
+    proxyReqOptDecorator: (proxyReqOpts, srcReq) => {
+      proxyReqOpts.headers["Content-Type"] = "application/json";
+      return proxyReqOpts;
+    },
+    userResDecorator: (proxyRes, proxyResData, userReq, userRes) => {
+      logger.info(
+        `Response received from Identity service: ${proxyRes.statusCode}`
+      );
+
+      return proxyResData;
+    },
+}));
+
+
+//setting of proxy for out post-services 
+app.use('/v1/posts',validateToken,proxy(process.env.POST_SERVICE_URL,{
+  ...proxyOptions,
+  proxyReqOptDecorator: (proxyReqOpts, srcReq) => {
+      proxyReqOpts.headers["Content-Type"] = "application/json";
+      proxyReqOpts.headers["x-user-id"] = srcReq.user.userId;
+
+      return proxyReqOpts;
+    },
+     userResDecorator: (proxyRes, proxyResData, userReq, userRes) => {
+      logger.info(
+        `Response received from Post service: ${proxyRes.statusCode}`
+      );
+
+      return proxyResData;
+    },
+}))
+
 
 app.use(errorHandler);
 
@@ -71,6 +104,9 @@ app.listen(PORT, () => {
   logger.info(`API GateWay is running on port ${PORT}`);
   logger.info(
     `Identity Service is running on port ${process.env.IDENTITY_SERVICE_URL}`
+  );
+   logger.info(
+    `Post Service is running on port ${process.env.POST_SERVICE_URL}`
   );
   logger.info(`Redis Url ${process.env.REDIS_URL}`);
 });
